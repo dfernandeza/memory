383a56ec3cdaddfa0cdee98e6e32b9ab826cde83
Daniel Fernandez <danyfernandeza@gmail.com>
2022-09-22 17:39:45 +0200

    fix: Improve icons

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,18 +7,6 @@ export const Layout: FC<{ children?: ReactNode }> = ({ children }) => {
       <Head>
         <title>Memory cards game!</title>
         <meta name="description" content="The memory cards game." />
-
-        {/* <!-- These meta tags are Apple-specific, and set the web application to run in full-screen mode with a black status bar. Learn more at https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/SafariHTMLRef/Articles/MetaTags.html--> */}
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-title" content="PWA Starter" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-
-        <link rel="apple-touch-icon" sizes="192x192" href="/icons/192.png" />
-        <link rel="icon" type="image/svg+xml" href="favicon.svg" />
-        <link rel="icon" type="image/png" href="favicon.png" />
-
-        {/* <!-- Imports the manifest to represent the web application. A web app must have a manifest to be a PWA. --> */}
-        <link rel="manifest" href="/manifest.json" />
       </Head>
 
       {children}
---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+
+        {/* <!-- These meta tags are Apple-specific, and set the web application to run in full-screen mode with a black status bar. Learn more at https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/SafariHTMLRef/Articles/MetaTags.html--> */}
+        <meta name="application-name" content="Memory" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Memory cards game" />
+        <meta name="description" content="The memory cards game." />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+
+        <link rel="apple-touch-icon" href="/icons/192.png" />
+        <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+        <link rel="icon" type="image/png" href="favicon.png" />
+        <link rel="shortcut icon" href="/favicon.png" />
+
+        {/* <!-- Imports the manifest to represent the web application. A web app must have a manifest to be a PWA. --> */}
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
