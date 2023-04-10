00fdb0e443f6d7788d3ebcb88965ba026ad6b4d2
Daniel Fernandez <danyfernandeza@gmail.com>
2023-04-10 17:21:58 +0200

    Migrate to Next.js 13

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,48 +22,56 @@ const Home: NextPage = () => {
 
         <h2>Choose a level</h2>
         <div className={styles.grid}>
-          <Link href="/levels/1">
-            <a className={styles.card} style={{ '--level-rotation': '2deg' }}>
-              <h3>6 cards</h3>
-              <article className={styles.levelGrid}>
-                {Array.from({ length: 6 }).map((_, i) => (
-                  <div key={`level-6--${i}`} />
-                ))}
-              </article>
-            </a>
+          <Link
+            href="/levels/1"
+            className={styles.card}
+            style={{ '--level-rotation': '2deg' }}
+          >
+            <h3>6 cards</h3>
+            <article className={styles.levelGrid}>
+              {Array.from({ length: 6 }).map((_, i) => (
+                <div key={`level-6--${i}`} />
+              ))}
+            </article>
           </Link>
 
-          <Link href="/levels/2">
-            <a className={styles.card} style={{ '--level-rotation': '-2deg' }}>
-              <h3>10 cards</h3>
-              <article className={styles.levelGrid}>
-                {Array.from({ length: 10 }).map((_, i) => (
-                  <div key={`level-10--${i}`} />
-                ))}
-              </article>
-            </a>
+          <Link
+            href="/levels/2"
+            className={styles.card}
+            style={{ '--level-rotation': '-2deg' }}
+          >
+            <h3>10 cards</h3>
+            <article className={styles.levelGrid}>
+              {Array.from({ length: 10 }).map((_, i) => (
+                <div key={`level-10--${i}`} />
+              ))}
+            </article>
           </Link>
 
-          <Link href="/levels/3">
-            <a className={styles.card} style={{ '--level-rotation': '2deg' }}>
-              <h3>16 cards</h3>
-              <article className={styles.levelGrid}>
-                {Array.from({ length: 16 }).map((_, i) => (
-                  <div key={`level-16--${i}`} />
-                ))}
-              </article>
-            </a>
+          <Link
+            href="/levels/3"
+            className={styles.card}
+            style={{ '--level-rotation': '2deg' }}
+          >
+            <h3>16 cards</h3>
+            <article className={styles.levelGrid}>
+              {Array.from({ length: 16 }).map((_, i) => (
+                <div key={`level-16--${i}`} />
+              ))}
+            </article>
           </Link>
 
-          <Link href="/levels/4">
-            <a className={styles.card} style={{ '--level-rotation': '-2deg' }}>
-              <h3>24 cards</h3>
-              <article className={styles.levelGrid}>
-                {Array.from({ length: 24 }).map((_, i) => (
-                  <div key={`level-24--${i}`} />
-                ))}
-              </article>
-            </a>
+          <Link
+            href="/levels/4"
+            className={styles.card}
+            style={{ '--level-rotation': '-2deg' }}
+          >
+            <h3>24 cards</h3>
+            <article className={styles.levelGrid}>
+              {Array.from({ length: 24 }).map((_, i) => (
+                <div key={`level-24--${i}`} />
+              ))}
+            </article>
           </Link>
         </div>
       </main>
